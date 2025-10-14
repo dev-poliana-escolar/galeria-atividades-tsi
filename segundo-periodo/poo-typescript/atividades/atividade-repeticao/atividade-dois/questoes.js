0e2fb3a5b8e63e9f7e440d90b235b248f1c00dde
dev-poliana-escolar <poliana.pinheiro@escolar.ifrn.edu.br>
2025-10-14 14:20:13 -0300

    feat: questoes um e dois

--- segundo-periodo/poo-typescript/atividades/atividade-repeticao/atividade-dois/questoes.js
+++ segundo-periodo/poo-typescript/atividades/atividade-repeticao/atividade-dois/questoes.js
@@ -3,11 +3,37 @@
 // determina o número de acertos daquele candidato.
 Object.defineProperty(exports, "__esModule", { value: true });
 const read = require("readline-sync");
-let num_questoes = Number(read.question("Quantidade de questões: "));
-let entradaGab_aluno = read.question("Gabarito do aluno: ");
-let entradaGab_prova = read.question("Gabarito da prova: ");
-let gab_prova = entradaGab_prova.split("");
-let gab_aluno = entradaGab_aluno.split("");
-// gab_aluno.forEach(resposta => {
-//     console.log(resposta)
-// })
+let op = read.question("op: ");
+if (op == 1) {
+    let num_questoes = Number(read.question("Quantidade de questões: "));
+    let gabarito = read.question("Gabarito da prova: ");
+    let respostas = read.question("Respostas do aluno: ");
+    let acertos = 0;
+    for (let i = 0; i < num_questoes; i++) {
+        if (gabarito[i] === respostas[i]) {
+            acertos++;
+        }
+    }
+    console.log(acertos);
+}
+else {
+    let cpf;
+    let entrada = read.question("Digite os 9 primeiros digitos do CPF: ");
+    cpf = entrada.split("").map(Number); // map converte o texto (split) para number
+    // --- Cálculo de d1 ---
+    let soma1 = 0;
+    for (let i = 0, peso = 10; i < 9; i++, peso--) {
+        soma1 += cpf[i] * peso;
+    }
+    let resto1 = soma1 % 11;
+    let d1 = (resto1 == 0 || resto1 == 1) ? 0 : (11 - resto1);
+    // --- Cálculo de d2 ---
+    let soma2 = 0;
+    for (let i = 0, peso = 11; i < 9; i++, peso--) {
+        soma2 += cpf[i] * peso;
+    }
+    soma2 += d1 * 2;
+    let resto2 = soma2 % 11;
+    let d2 = (resto2 == 0 || resto2 == 1) ? 0 : (11 - resto2);
+    console.log(`Dígitos verificadores: ${d1}${d2}`);
+}
--- segundo-periodo/poo-typescript/atividades/atividade-repeticao/atividade-dois/questoes.ts
+++ segundo-periodo/poo-typescript/atividades/atividade-repeticao/atividade-dois/questoes.ts
@@ -1,22 +1,49 @@
 // escrever um programa que, dado o gabarito e as respostas de um candidato,
 // determina o número de acertos daquele candidato.
 
-const read =require("readline-sync");
 
 
+const read = require("readline-sync");
 
-let num_questoes : number = Number(read.question("Quantidade de questões: "));
-let entradaGab_aluno : string = read.question("Gabarito do aluno: ");
-let  entradaGab_prova : string = read.question("Gabarito da prova: ");
+let op: number = read.question("op: ")
+if (op ==1){
+    let num_questoes: number = Number(read.question("Quantidade de questões: "));
+    let gabarito: string = read.question("Gabarito da prova: ");
+    let respostas: string = read.question("Respostas do aluno: ");
 
-let gab_prova : string[] = entradaGab_prova.split("");
-let gab_aluno : string[] = entradaGab_aluno.split("");
+    let acertos: number = 0;
 
-// gab_aluno.forEach(resposta => {
-//     console.log(resposta)
+    for (let i = 0; i < num_questoes; i++) {
+    if (gabarito[i] === respostas[i]) {
+        acertos++;
+    }
+    }
 
-// })
+    console.log(acertos);
+}else{
+    let cpf: number[];
+    let entrada = read.question("Digite os 9 primeiros digitos do CPF: ");
+    cpf = entrada.split("").map(Number); // map converte o texto (split) para number
 
+    // --- Cálculo de d1 ---
+    let soma1 = 0;
+    for (let i = 0, peso = 10; i < 9; i++, peso--) {
+    soma1 += cpf[i] * peso;
+    }
 
+    let resto1 = soma1 % 11;
+    let d1 = (resto1 == 0 || resto1 == 1) ? 0 : (11 - resto1);
 
+    // --- Cálculo de d2 ---
+    let soma2 = 0;
+    for (let i = 0, peso = 11; i < 9; i++, peso--) {
+    soma2 += cpf[i] * peso;
+    }
+    soma2 += d1 * 2; 
 
+    let resto2 = soma2 % 11;
+    let d2 = (resto2 == 0 || resto2 == 1) ? 0 : (11 - resto2);
+
+    console.log(`Dígitos verificadores: ${d1}${d2}`);
+
+}
