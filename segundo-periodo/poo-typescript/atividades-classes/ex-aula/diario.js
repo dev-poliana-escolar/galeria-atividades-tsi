75d724efa71aab7ca1e0d9c22daf68d00182d5d9
dev-poliana-escolar <poliana.pinheiro@escolar.ifrn.edu.br>
2025-11-11 12:23:46 -0300

    feat: media final com inserção de nota final

--- segundo-periodo/poo-typescript/atividades-classes/ex-aula/diario.js
+++ segundo-periodo/poo-typescript/atividades-classes/ex-aula/diario.js
@@ -21,6 +21,7 @@ class RegistroNotas {
     #situacao_estudante = Situacao.cursando;
     #primeira_nota;
     #segunda_nota;
+    #nota_avaliacao_final;
     #media_parcial = undefined;
     #media_final = undefined;
     constructor(nome_est, matricula, notaUm, notaDois) {
@@ -48,40 +49,87 @@ class RegistroNotas {
     getMediaParcial() {
         if (this.#primeira_nota != 0 && this.#segunda_nota != 0) {
             this.#media_parcial = ((this.#primeira_nota * 2) + (this.#segunda_nota * 3)) / 5;
-            return this.#media_parcial;
+            let situacao_final = this.#situacao_estudante;
+            if (this.#media_parcial >= 60) {
+                situacao_final = Situacao.aprovado;
+            }
+            else if (this.#media_parcial >= 10) {
+                situacao_final = Situacao.prova_final;
+            }
+            else {
+                situacao_final = Situacao.reprovado;
+            }
+            return {
+                media_parcial: this.#media_parcial,
+                situacao: situacao_final
+            };
         }
         else {
             return undefined;
         }
     }
-    getMediaFinal() {
-        if (this.#media_parcial !== undefined) {
-            let situacao_final;
-            if (this.#media_parcial >= 60) {
-                situacao_final = this.#situacao_estudante = Situacao.aprovado;
-            }
-            else if (this.#media_parcial >= 10) {
-                situacao_final = this.#situacao_estudante = Situacao.prova_final;
+    getMediaFinal(nota_final) {
+        this.getMediaParcial();
+        if (this.#media_parcial === undefined) {
+            return "Não tem média parcial";
+        }
+        let situacao_final;
+        let mf;
+        if (this.#media_parcial >= 60) {
+            this.#media_final = this.#media_parcial;
+            situacao_final = Situacao.aprovado;
+        }
+        else if (this.#media_parcial >= 10) {
+            if (nota_final === undefined) {
+                this.#media_final = undefined;
+                situacao_final = Situacao.prova_final;
             }
             else {
-                situacao_final = this.#situacao_estudante = Situacao.reprovado;
+                mf = (nota_final + this.#media_parcial) / 2;
+                this.#media_final = mf;
+                if (mf >= 60) {
+                    situacao_final = Situacao.aprovado;
+                }
+                else {
+                    situacao_final = Situacao.reprovado;
+                }
             }
-            this.#media_final = this.#media_parcial;
-            return this.#media_final;
         }
         else {
-            return "Não tem parcial";
+            this.#media_final = this.#media_parcial;
+            situacao_final = Situacao.reprovado;
         }
+        this.#situacao_estudante = situacao_final;
+        this.#nota_avaliacao_final = nota_final;
+        return {
+            media_final: this.#media_final,
+            situacao: situacao_final,
+            nota_prova_final: this.#nota_avaliacao_final
+        };
     }
     getRegistros() {
-        return `Nome estudante:${this.#nome_estudante} - Situação: ${this.#situacao_estudante}`;
+        this.getMediaParcial();
+        this.getMediaFinal(this.#nota_avaliacao_final);
+        return {
+            nome: this.#nome_estudante,
+            matricula: this.#matricula,
+            primeira_nota: this.#primeira_nota,
+            segunda_nota: this.#segunda_nota,
+            nota_prova_final: this.#nota_avaliacao_final ? this.#nota_avaliacao_final : '-',
+            media_parcial: this.#media_parcial,
+            media_final: this.#media_final ? this.#media_final : "-",
+            situacao: this.#situacao_estudante
+        };
     }
 }
-let reg_um = new RegistroNotas("Poliana", '202546777', 70, 0);
-console.log(reg_um.getMediaParcial());
-console.log(reg_um.getMediaFinal());
-console.log(reg_um.setSituacao(Situacao.trancado));
+let reg_um = new RegistroNotas("Ana", '202546777', 70, 70);
+let reg_dois = new RegistroNotas("Alisson", '202546888', 80, 2);
+let reg_tres = new RegistroNotas("Tamires", '202546999', 20, 60);
+let reg_quatro = new RegistroNotas("Pedro", '202546101', 65.8, 89);
+let reg_cinco = new RegistroNotas("Hellen", '202546555', 80, 76);
 console.log(reg_um.getRegistros());
+reg_tres.getMediaFinal(60);
+console.log(reg_tres.getRegistros());
 class Diario {
     #nome_disciplina;
     #docente;
@@ -112,3 +160,4 @@ class Diario {
         this.#curso = novoCurso;
     }
 }
+let diarioUm = new Diario("Programação Orientada a Objetos", "Bruno", Turno.vespertino, "TSI", "2025", 120, [reg_um, reg_dois]);
