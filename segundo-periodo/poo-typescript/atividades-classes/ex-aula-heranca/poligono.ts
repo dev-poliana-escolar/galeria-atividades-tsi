a5757000551f27a6632201abb72711cbf3282f3f
dev-poliana-escolar <poliana.pinheiro@escolar.ifrn.edu.br>
2026-01-22 17:49:41 -0300

    wip: exemplo de class poligono

--- segundo-periodo/poo-typescript/atividades-classes/ex-aula-heranca/poligono.ts
+++ segundo-periodo/poo-typescript/atividades-classes/ex-aula-heranca/poligono.ts
@@ -1,18 +1,38 @@
-export class Poligono{
+export abstract class Poligono{
     protected __numLados : number;
+    protected lados :number[] | undefined;
     constructor(){
         this.__numLados = 0
+        this.lados = undefined;
     }
 
     get numLados(): number{
         return this.__numLados;
     }
+
+    informarMedidasLados(...lados:number[]){
+        if (lados.length == this.__numLados){
+            this.lados = lados;
+            return true
+        }
+
+        return false
+    }
+    abstract calcularArea(): number|undefined;
+
 }
 
 class Triangulo extends Poligono{
     constructor(){
         super()
         this.__numLados = 3
+        this.informarMedidasLados(...lados)
+    }
+
+    calcularArea(): number | undefined {
+       if (this.lados && this.lados.length){
+            
+       }
     }
 }
 
